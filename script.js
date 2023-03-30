a7ae1ebfd2db2aa3554fd4fe553cbbd8865a559c
Aaxtrox <pawelw@gmx.com>
2023-03-30 11:42:24 +0100

    2 buttons after searchbar done

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // select buttons
 var restore = document.getElementById("window-restore");
 var close = document.getElementById("window-close");
+var all = document.getElementById("color-change-all");
+var other = document.getElementById("color-change-other");
 
 
 //add event listener on click to restore button
@@ -16,4 +18,16 @@ restore.addEventListener("click", function() {
 //add event listener on click to close button
 close.addEventListener("click", function() {
     window.location.href = "https://www.google.com";
+});
+
+//add event listener on click all span
+all.addEventListener("click", function() {
+    document.getElementById("color-change-all").style.color = "tomato";
+    document.getElementById("color-change-other").style.color = "black";
+});
+
+//add event listener on click other span
+other.addEventListener("click", function() {
+    document.getElementById("color-change-other").style.color = "tomato";
+    document.getElementById("color-change-all").style.color = "black";
 });
